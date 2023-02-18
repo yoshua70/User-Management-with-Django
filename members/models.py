e60749174d51baab07d567f4337bc862f1e31ba8
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-02-19 00:10:54 +0100

    feat: create account and database models

--- members/models.py
+++ members/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class DataBase(models.Model):
+    label = models.CharField(max_length=100)
+
+
+class Account(models.Model):
+    email = models.CharField(max_length=100)
+    db = models.ForeignKey(DataBase, on_delete=models.SET_NULL, null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True
+    )
