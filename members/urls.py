b1828a74b5131272e330ac06458ab2b411ff56f7
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-02-18 12:07:49 +0100

    feat: set login page as homepage
    Create `login_user` view.
    Set url root pattern to point to  members.urls in global urls.
    Add url root pattern with login_user as view to members.urls.

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('')
+    path('', views.login_user, name="homepage"),
+    path('login_user', views.login_user, name="login"),
 ]
--- user_management/urls.py
+++ user_management/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('members.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'))
 ]
